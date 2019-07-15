47667e25f755730d3bed92938ebb304d0f939306
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-14 18:42:48 -0600

    If-Else statements

--- ConditionalChecks/main.cpp
+++ ConditionalChecks/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	string password = "hello";
+	/*string password = "hello";
 	cout << "Enter your password > " << flush;
 
 	string input;
@@ -16,6 +16,23 @@ int main() {
 	}
 	if (input != password) {
 		cout << "Access denied." << endl;
+	}*/
+
+	cout << "1.\tAdd new record." << endl;
+	cout << "2.\tDelete record." << endl;
+	cout << "3.\tView record." << endl;
+	cout << "4.\tSearch record." << endl;
+	cout << "5.\tQuit." << endl;
+
+	cout << "Enter your Selection > " << flush;
+	int value;
+	cin >> value;
+
+	if (value < 3) {
+		cout << "Insufficient privileges to use this menu option." << endl;
+	}
+	else {
+		cout << "Privilege level sufficient." << endl;
 	}
 
 	return 0;
